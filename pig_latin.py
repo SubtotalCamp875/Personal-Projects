26a4f0d41d5501a5c1bcaff792a68e70c9c8e6c0
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-03-25 01:47:15 +0000

    Sat, Mar 25, 2023, 1:47 AM +00:00

--- pig_latin.py
+++ pig_latin.py
@@ -1,13 +1,13 @@
 def main():
     english = input("Enter a sentence in English: ")
-    englist, platin = english.split(" "), ""
+    englist, platin, punctuation = english.split(" "), "", [".", ",", "!", "?", ";", ":", '"', "'", "(", ")", "[", "]"]
     for i in range(len(englist)):
         prefix = ""
         if englist[i][0].lower().strip not in ["a", "e", "i", "o", "u"]:
             prefix = englist[i][0]
         englist_index = str(englist[i]).replace(prefix, "", 1) + prefix + "ay"
 
-        match 
+        match
         platin = platin + englist_index + " "
     print(platin)
 
