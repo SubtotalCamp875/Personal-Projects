fb62f99164c88044628d47863b7388ff7214de6a
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-03-25 19:14:09 +0000

    Sat, Mar 25, 2023, 7:14 PM +00:00

--- decryption.py
+++ decryption.py
@@ -1,2 +1,8 @@
-dict = dict(zip(range(25)), zip("abcdefghijklmnopqrstuvwxyz"))
-print(dict[1])
+dict = dict(zip(range(25), "abcdefghijklmnopqrstuvwxyz"))
+print(dict["a"])
+
+"""
+for i in range(len(x)):
+    for j in range(len(x[i])):
+        x[i][j] = x.replace(j)
+"""
