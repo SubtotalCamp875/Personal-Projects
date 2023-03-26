e83908cbb2ecb3c80b75ada2ba6ce399a3943d01
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-03-26 16:37:26 +0000

    Sun, Mar 26, 2023, 4:37 PM +00:00

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-Where you ___ a angular witch, _ ___ many l_gh__.
-A__ _ho__ lurking on the twilight o_ __ __ some kind of weird dark magic?
+dict = {"Baja Taco": 4.00, "Burrito": 7.50,"Bowl": 8.50,"Nachos": 11.00,"Quesadilla": 8.50,"Super Burrito": 8.50,"Super Quesadilla": 9.50,"Taco": 3.00,"Tortilla Salad": 8.00}
+cost = 0
+item = input("Item: ")
+cost += dict[item]
