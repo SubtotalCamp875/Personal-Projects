c9c09a460f965d7d76557ecbb6de73fd72d99e44
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-04-01 00:19:59 +0000

    Sat, Apr 1, 2023, 12:19 AM +00:00

--- depot.py
+++ depot.py
@@ -1,7 +1,13 @@
 def main():
     selection = input(f"Please select a department: \n1. Paint\n2. Flooring\n3. Lighting\n").strip().lower()
-    if selection in ["1", "paint"]:
-        area = input("Please input integer values )
+    match selection:
+        case "1" | "paint":
+            area = input('Please input values in forms of "1x2": ').replace(" ", "").split("x")
+            wall = input("How many integer walls are you painting? ")
+        case "2" | "flooring": pass
+        case "3" | "lighting": pass
+
+
 
 
 
