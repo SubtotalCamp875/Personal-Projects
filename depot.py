17823ca3a9c146e87b71f0cf691a201deec82c69
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-04-01 00:29:47 +0000

    Sat, Apr 1, 2023, 12:29 AM +00:00

--- depot.py
+++ depot.py
@@ -5,6 +5,8 @@ def main():
             case "1" | "paint":
                 area = input('Please input values in forms of "1x2": ').replace(" ", "").split("x")
                 area = float(input("How many walls are you painting? ")) * float(area[0]) * float(area[1])
+                cans = (area/x)
+                print(f"You will need {cans} cans of paint which cost $25 each. \nYour total cost will be {cans*25}.\n")
             case "2" | "flooring": pass
             case "3" | "lighting": pass
     except IndexError: print("You provided invalid syntax")
@@ -14,4 +16,4 @@ def main():
 
 
 
-print(main())
+main()
