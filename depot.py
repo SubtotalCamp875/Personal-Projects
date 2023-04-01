59b75b3bb1ea2efdd50ea705ebf7e3df7feaeded
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-04-01 00:45:59 +0000

    Sat, Apr 1, 2023, 12:45 AM +00:00

--- depot.py
+++ depot.py
@@ -6,12 +6,12 @@ def main():
     match selection:
         case "1" | "paint":
             try:
-                area = input('Please input values in forms of "1x2" inches: ').replace(" ", "").split("x")
-                area = float(input("How many walls will you be painting? ")) * float(area[0]) * float(area[1])
-                cans = math.ceil(area/x)
+                length, walls = input('Please input values in forms of "1x2" inches: ').replace(" ", "").split("x"), float(input("How many walls will you be painting? "))
+                area = walls * float(length[0]) * float(length[1])
+                cans = math.ceil(area/350)
             except IndexError: print("You provided invalid syntax.")
-            print(f"You will need {cans} cans of paint for {area} inches of wall. \nEach can of paint will cost $25. \nYour total cost will be {cans*25}.\nYou will have {cans*size-area} inches of paint left over")
-            
+            print(f"You will need {cans} cans of paint for {area*walls} square inches of wall. \nEach can of paint will cost $25. \nYour total cost will be {cans*25}.\nYou will have {cans*size-area} inches of paint left over")
+
         case "2" | "flooring":
             pass
         case "3" | "lighting":
