3046f2c58cc6a2d41cf9213b5a136f73eaf365c3
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-04-01 00:41:48 +0000

    Sat, Apr 1, 2023, 12:41 AM +00:00

--- depot.py
+++ depot.py
@@ -11,6 +11,7 @@ def main():
                 cans = math.ceil(area/x)
             except IndexError: print("You provided invalid syntax.")
             print(f"You will need {cans} cans of paint for {area} inches of wall. \nEach can of paint will cost $25. \nYour total cost will be {cans*25}.\nYou will have {cans*size-area} inches of paint left over")
+            
         case "2" | "flooring":
             pass
         case "3" | "lighting":
