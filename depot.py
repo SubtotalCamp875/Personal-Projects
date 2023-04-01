2350d53fca6759b4b06918673812e8394322c1f4
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-04-01 00:25:16 +0000

    Sat, Apr 1, 2023, 12:25 AM +00:00

--- depot.py
+++ depot.py
@@ -1,12 +1,15 @@
 def main():
     selection = input(f"Please select a department: \n1. Paint\n2. Flooring\n3. Lighting\n").strip().lower()
-    match selection:
-        case "1" | "paint":
-            area = input('Please input values in forms of "1x2": ').replace(" ", "").split("x")
-            wall = input("How many integer walls are you painting? ")
-        case "2" | "flooring": pass
-        case "3" | "lighting": pass
+    try:
+        match selection:
+            case "1" | "paint":
+                area = input('Please input values in forms of "1x2": ').replace(" ", "").split("x")
+                area = float(input("How many walls are you painting? ")) * float(area[0]) * float(area[1])
+            case "2" | "flooring": pass
+            case "3" | "lighting": pass
+    except IndexError: print("You provided invalid syntax")
 
+    return(area)
 
 
 
