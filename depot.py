1a9520b03a884345b682bbb88ea44c9cc9f486d2
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-04-01 01:12:18 +0000

    Sat, Apr 1, 2023, 1:12 AM +00:00

--- depot.py
+++ depot.py
@@ -6,14 +6,15 @@ def main():
     match selection:
         case "1" | "paint" | "one":
             try:
-                length, walls = input('Please input values in forms of "1x2" inches: ').replace(" ", "").split("x"), float(input("How many walls will you be painting? "))
+                length, walls = input('What is the length adn iwtPlease input values in forms of "1x2" inches: ').replace(" ", "").split("x"), float(input("How many walls will you be painting? "))
                 area = walls * float(length[0]) * float(length[1])
                 cans = math.ceil(area/350)
             except IndexError: print("You provided invalid syntax.")
             return(print(f"\nYou will need {cans} cans of paint for {area} square inches of wall. \nEach can of paint will cost $25 and cover 350 square inches. \nYour total cost will be {cans*25}.00 dollars.\nYou will have {cans*350-area} square inches of paint left over.\n"))
         case "2" | "flooring" | "two":
             try:
-                length, flooring = input('Please input values in forms of "1x2" inches: ').replace(" ", "").split("x"), float(input("What kind of flooring would you like? \n1. 10in x 10in Tiles\n2. Carpet\nSelection: "))
+                length, flooring = input('Please input values in forms of "1x2" inches: ').replace(" ", "").split("x"), input("What kind of flooring would you like? (Please enter an integer)\n1. 10in x 10in Tiles\n2. Carpet\nSelection: ")
+
             except: pass
         case "3" | "lighting" | "three":
             pass
