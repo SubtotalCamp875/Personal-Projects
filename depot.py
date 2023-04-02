ef7401a7ffb9e4f28d99b6c7afabcc00f0ddfa20
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-04-02 15:16:36 +0000

    Sun, Apr 2, 2023, 3:16 PM +00:00

--- depot.py
+++ depot.py
@@ -1,4 +1,5 @@
 #total time spent: 1hr 30min
+#11:10
 import math
 
 def main():
@@ -20,7 +21,7 @@ def main():
                 length, flooring = input('What is the length and width of your floor? Please input values in forms of "1x2" inches: ').replace(" ", "").split("x"), str(input("What kind of flooring would you like? (Please enter an integer)\n1. 10in. by 10in. Tiles\n2. Carpet\nSelection: ")).lower().strip()
                 area = int(length[0]) * int(length[1])
                 match flooring:
-                    case "1" | "one": #10 by 10 floor
+                    case "1": #10 by 10 floor
                         tiles = area/100
                         return(print(f"\nYou will need {tiles} of the 10in by 10in Tiles.\nIndividaul tiles will cost $5.00 so you would need ${tiles*5} worth of tiles. \n"))
                     case "2" | "two": #Carpets
@@ -29,7 +30,11 @@ def main():
 
 
         case "3" | "lighting" | "three":
-            pass
+            light, amount = input("What type of light would you like? (Please select an integer)\n1. Cieling light\n2. Wall light\nSelection: "), input("How many of those lights would you need? ")
+            if light == "1": return(print(f"Each light wull cost $5.00 so your total cost for {amount} lights will be ${int(amount)*5}.00"))
+            if light == "2": return(print(f"Each light wull cost $15.00 so your total cost for {amount} lights will be ${int(amount)*15}.00"))
+
+
 
 
 main()
