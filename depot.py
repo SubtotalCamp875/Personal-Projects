dd245f603ef9924fb994506dcc2064b288c435a3
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-04-02 16:02:58 +0000

    Sun, Apr 2, 2023, 4:02 PM +00:00

--- depot.py
+++ depot.py
@@ -23,27 +23,30 @@ def Main():
                 if flooring == "1": return(print(f"\nYou will need {area/100} of the 10in by 10in Tiles.\nIndividaul tiles will cost ${cost}.00 so you would need ${(area/100)*{cost}} worth of tiles. \n"))
                 if flooring == "2": pass
                 if "-" in [flooring, length]: return(print("The input must not be negative!\n"))
-                else: return(print("The choise you entered was not listed above\n"))
+                else: return(print("The choice you entered was not listed above\n"))
 
 
             case "3" | "lighting" | "three":
                 amount, light= input("How many of those lights would you need? "), input("What type of light would you like? (Please select an integer)\n1. Cieling light\n2. Wall light\nSelection: ")
 
-                if amount == float: return(print(f"You can not get {amount} of a light!"\n))
+                if amount == float: return(print(f"You can not get {amount} of a light!\n"))
                 if light == "1": return(print(f"Each light wull cost $5.00 so your total cost for {amount} lights will be ${int(amount)*5}.00"))
                 if light == "2": return(print(f"Each light wull cost $15.00 so your total cost for {amount} lights will be ${int(amount)*15}.00"))
                 if "-" in [light, amount]: return(print("The input must not be negative!\n"))
-                else: return(print("The choise you entered was not listed above\n"))
+                else: return(print("The choice you entered was not listed above\n"))
 
-            case _ : return(print("The choise you entered was not listed above\n"))
+            case _ : return(print("The choice you entered was not listed above\n"))
     except IndexError: return(print("You provided wrong syntax\n"))
     except SyntaxError: return(print("You provided wrong syntax\n"))
 
 
 def ExitStatement():
-     = input("You just pressed the button to end the code! What Exiting Statement would you like to hear?\n 1. Formal\n2. Informal\nSelection: ")
-    match selection =
-
+    statement = input("You pressed hte button to end the code! Before you leave, allow us to say our farewell.\n1. Formal\n2.Informal\nSelection: ").lower().strip()
+    match statement:
+        case "1" | "one" | "formal": return(print("Thank You for shopping at Home Depot! Please come back next time."))
+        case "2" | "two" | "informal":
+        case _ : return(print("Your choice was not an option. How did you mess up at a time like this? Please try again...))
+            
 
 
 
