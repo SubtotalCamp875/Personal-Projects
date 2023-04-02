064988f73028423d49d5b5076313db2fa1eea3c7
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-04-02 15:38:58 +0000

    Sun, Apr 2, 2023, 3:38 PM +00:00

--- depot.py
+++ depot.py
@@ -10,6 +10,8 @@ def main():
 
             case "1" | "paint" | "one": #Painting
                 length, walls = input('What is the length and width of your wall? Please input values in forms of "1x2" inches: ').replace(" ", "").split("x"), float(input("How many walls will you be painting? "))
+                if "-" in [flooring, length]:
+                    return(print("The input must not be negative!\n"))
                 area = walls * float(length[0]) * float(length[1])
                 cans = math.ceil(area/350)
                 return(print(f"\nYou will need {cans} cans of paint for {area} square inches of wall. \nEach can of paint will cost $25.00 and cover 350 square inches. \nYour total cost will be {cans*25}.00 dollars.\nYou will have {cans*350-area} square inches of paint left over.\n"))
@@ -23,6 +25,8 @@ def main():
                     return(print(f"\nYou will need {tiles} of the 10in by 10in Tiles.\nIndividaul tiles will cost $5.00 so you would need ${tiles*5} worth of tiles. \n"))
                 if flooring == "2":
                     pass
+                if "-" in [flooring, length]:
+                    return(print("The input must not be negative!\n"))
                 else: return(print("The choise you entered was not listed above\n"))
 
 
@@ -30,6 +34,8 @@ def main():
                 amount, light = input("How many of those lights would you need? "), input("What type of light would you like? (Please select an integer)\n1. Cieling light\n2. Wall light\nSelection: ")
                 if light == "1": return(print(f"Each light wull cost $5.00 so your total cost for {amount} lights will be ${int(amount)*5}.00"))
                 if light == "2": return(print(f"Each light wull cost $15.00 so your total cost for {amount} lights will be ${int(amount)*15}.00"))
+                if "-" in [light, amount]:
+                    return(print("The input must not be negative!\n"))
                 else: return(print("The choise you entered was not listed above\n"))
 
             case _ : return(print("The choise you entered was not listed above\n"))
--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-#a
+x = "-4"
+if "-" in x:
+    print("neg")
+print("done")
