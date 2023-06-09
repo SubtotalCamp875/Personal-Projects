eded4cf710383eaf24cabfba8725dfef27f79199
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-06-09 15:48:40 +0000

    Fri, Jun 9, 2023, 3:48 PM +00:00

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-def main():
-    sign_dummy, a, steps = [], [], ["1","1.5","2","2.5","3","3.5","4"]
-    notes = dict(zip("abcdefg",steps))
-    a_dummy = input("a: ")
-    for i in range(len(a_dummy)):
-        print(a_dummy[i])
-        print(notes["b"])
-        if a_dummy[i] in ["a","b","c","d","e","f","g"]: a.append(notes[a[i]])
-        else: return(print("please input notes"))
-
-main()
+x = -1
+if x != abs(x):
+    print(1)
+else: print(2)
