4a9043051f2fd4a8ade61493b94ba1960015f77c
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-06-09 14:03:30 +0000

    Fri, Jun 9, 2023, 2:03 PM +00:00

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-notes = dict(zip("abcdefg","1234567"))
-x = "abcdefg"
-for i in range(len(x)):
-    print(notes[x[i]])
+x = dict(zip("abcdefg","1234567"))
+a = "abcd"
+for i in range(len(a)):
+    if a[i] == ["a","b","c","d","e","f","g"]:
+        a[i] = list(x[a[i]])
+
+print(a)
