3306515a9366ac0b391e8caade4c178e84149ee6
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-06-09 14:36:35 +0000

    Fri, Jun 9, 2023, 2:36 PM +00:00

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-values = ["1","1.5","2","2.5","3","3.5","4"]
-x = dict(zip("abcdefg",values))
-a = "abcd"
-a_dummy = []
-
-for i in range(len(a)):
-    if a[i] in ["a","b","c","d","e","f","g"]:
-        a_dummy.append(x[a[i]])
-
-def request_input():
-    sign, a, b = list(input("sign (1=+, 0=-): ")), str(input("input of change in height: ")).replace(" ","").split(","), str(input("input of distance: ")).replace(" ","").split(",")
-    return(sign, a, b)
-
-a, b = request_input()
-print(a)
+for i in range(10):
+    print(i)
