4f51673d80a0672d0efd96308f9153d80bf90dce
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-06-09 15:36:31 +0000

    Fri, Jun 9, 2023, 3:36 PM +00:00

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-x = 2
-match x:
-    case 1:
-        print("1")
-    case _:
-        print("other")
+def main():
+    sign_dummy, a, steps = [], [], ["1","1.5","2","2.5","3","3.5","4"]
+    notes = dict(zip("abcdefg",steps))
+    a_dummy = input("a: ")
+    for i in range(len(a_dummy)):
+        print(a_dummy[i])
+        if a_dummy[i] in ["a","b","c","d","e","f","g"]: a.append(notes[a[i]])
+        else: return(print("please input notes"))
+
+main()
