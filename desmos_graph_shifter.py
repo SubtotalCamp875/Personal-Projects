0f06c6cc77a82c7e8f456940d8a881315b952529
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-06-10 17:17:32 +0000

    Sat, Jun 10, 2023, 5:17 PM +00:00

---
+++ desmos_graph_shifter.py
@@ -0,0 +1,66 @@
+def main():
+
+    func = int(input("1 = Coordinate Pairs, 2 = Shift Horizontal, 3 = Create W. Notes: "))
+    steps = ["0.5","1","1.5","2","2.5","3","3.5"]
+    notes = dict(zip("abcdefg",steps))
+
+    match func:
+
+        case 1:
+            sign, a, b = request_input()
+            sign = sign_list(sign)
+            for i in range(len(a)): print(f"{sign[i]}{a[i]},{b[i]}")
+
+        case 2:
+            sign, a, b = request_input()
+            sign = sign_list(sign)
+            unchanged, adder = int(input("How many do you want unchanged: ")), int(input("How much do you want to shift (+x/-x): "))
+            print("\ny_{1}=",end="")
+            for i in range(unchanged):
+                print(f"{sign[i]}g\left(x,{a[i]},{b[i]}\ right)".replace(" ",""),end = "")
+
+            for i in range(len(a)-unchanged):
+                i += unchanged
+                print(f"{sign[i]}g\left(x,{a[i]},{float(b[i])+adder}\ right)".replace(" ","").replace(".0",""),end = "")
+            print(("\ng\left(x,n,t\ "+"right)\ =\ \ "+"frac{n}{1+e^{60\left(-x+.5t\ "+"right)}}").replace(" r","r").replace(" f","f"))
+
+        case 3:
+            sign, a, a_dummy = "1", [], str(input("Input notes (ex. a,b,c,d): ")).replace(" ","").split(",")
+            for i in range(len(a_dummy)):
+
+                #converts a-z >> #
+                if a_dummy[i] in ["a","b","c","d","e","f","g"]: a.append(float(notes[a_dummy[i]]))
+                else: return(print("please input notes"))
+
+                #find differnce between value and prev value
+                if i == 0: continue
+                else:
+                    a[i] = float(notes[a_dummy[i-1]])-a[i]
+                    if a[i] == -abs(a[i]):
+                        a[i] = abs(a[i])
+                        sign += "1"
+                    else: sign += "0"
+            print(f'The difference in value is {str(a).replace(".0", "")} and sign = {sign}')
+    print(" ")
+
+
+def request_input():
+    sign, a, b = list(input("Sign (1=+, 0=-)(ex. 10110): ")), str(input("Input of change in height (ex. 1,1,2,5): ")).replace(" ","").split(","), str(input("Input of distance (ex. 2,5,8): ")).replace(" ","").split(",")
+    return(sign, a, b)
+
+
+def sign_list(sign):
+    sign_dummy = []
+    for i in range(len(sign)):
+        sign_dummy.append(sign_assign(sign,i))
+    return(sign_dummy)
+
+
+def sign_assign(sign,i):
+    if sign[i] == "1": return("+")
+    if sign[i] == "0": return("-")
+
+
+
+if __name__ == "__main__":
+    main()
