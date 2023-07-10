6fe1c3e279d5c813c8639ae00174747fef3d2a1d
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-10 16:29:22 +0000

    Mon, Jul 10, 2023, 4:29 PM +00:00

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
 steps, notes_list  = ['10','11','12','13','1','2','3','4','5','6','7','8','9'], ['a', 'a#', 'b', 'b#', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
 notes = dict(zip(notes_list,steps))
-x = "a"
-print(notes(x))
+print(notes('10'))
