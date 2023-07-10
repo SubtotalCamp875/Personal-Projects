ac0bff27e2f77daaddc7dd40db1324ec253cf90a
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-10 21:25:33 +0000

    Mon, Jul 10, 2023, 9:25 PM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -1,5 +1,5 @@
 def main():
-    func = int(input('Graph maker = 1, Syntax Pairing = 2, Coordinate Pairs = 3, Conversion = 4, Help/Explanation = : '))
+    func = int(input('Graph maker = 1, Syntax Pairing = 2, Coordinate Pairs = 3, Conversion = 4, Differences = 5, Help/Explanation = : '))
     answer = "y_{ 1}=".replace(' ','')
     steps, notes_list  = ['10','11','12','13','1','2','3','4','5','6','7','8','9'], ['a', 'a#', 'b', 'b#', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
     notes = dict(zip(notes_list,steps))
