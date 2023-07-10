de2c8afd2d3f5f6ce3adf71dfbbb661a58be0d16
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-10 20:39:36 +0000

    Mon, Jul 10, 2023, 8:39 PM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -1,5 +1,5 @@
 def main():
-    func = int(input('Graph maker = 1, Pairing = 2, Help/Explanation = 3: '))
+    func = int(input('Graph maker = 1, Syntax Pairing = 2, Coordinate Pairs = 3, Help/Explanation = : '))
     answer = "y_{ 1}=".replace(' ','')
     steps, notes_list  = ['10','11','12','13','1','2','3','4','5','6','7','8','9'], ['a', 'a#', 'b', 'b#', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
     notes = dict(zip(notes_list,steps))
@@ -18,8 +18,14 @@ def main():
             print(f'\n{answer}\n')
             print(('g\left(x,n,t\ '+'right)\ =\ \ '+'frac{n}{1+e^{60\left(-x+t\ '+'right)}}\n').replace(' r','r').replace(' f','f'))
 
-        case 2: pass
-        case 3: pass
+        case 2:
+            pair, x, y = '', input('Enter list of First value (1,2,3,4,5): ').replace(' ','').split(','), input('Enter list of Second value (1,2,3,4,5): ').replace(' ','').split(',')
+            for i in range(len(x)): pair += f'{x[i]} {y[i]},'
+            print(pair[0:-1])
+
+        case 3:
+            pair, x, y = '', input('Enter list of First value (1,2,3,4,5): ').replace(' ','').split(','), input('Enter list of Second value (1,2,3,4,5): ').replace(' ','').split(',')
+            for i in range(len(x)): print(f'{x[i]}, {y[i]}')
 
 if __name__ == '__main__':
     main()
