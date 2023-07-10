b0240c42eb65701447da793c66e0e7d1a3c460b4
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-10 16:26:21 +0000

    Mon, Jul 10, 2023, 4:26 PM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -1,18 +1,16 @@
 def main():
     func = int(input('Graph maker = 1, Help/Explanation = 2: '))
     answer = "y_{ 1}=".replace(' ','')
-    steps = ['10','11','12','13','1','2','3','4','5','6','7','8','9'],
-    notes_list =['a', 'a#', 'b', 'b#', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#'],
-    notes_list_upper = ['A', 'A#', 'B', 'B#', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    steps, notes_list  = ['10','11','12','13','1','2','3','4','5','6','7','8','9'], ['a', 'a#', 'b', 'b#', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
     notes = dict(zip(notes_list,steps))
 
     match func:
         case 1:
-            values = input('Input n and t in forms of "n t,n t" etc: ').split(',')
+            values = input('Input n and t in forms of "n t,-n -t" etc: ').split(',')
             for i in range(len(values)):
                 values[i] = values[i].split(' ')
-                if values[i] in [notes_list, notes_list_upper]:
-                    value[i] = notes(values[i].lower())
+                if values[i] in notes_list:
+                    value[i] = notes(values[i])
                 answer += f'+g\left(x,{values[i][0]},{values[i][1]}\ right)'.replace(' ','')
             print(f'\n{answer}\n')
 
