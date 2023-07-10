6b4482b96cabf8f1f278ec630f3d767bbf69a7ee
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-10 16:12:13 +0000

    Mon, Jul 10, 2023, 4:12 PM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -1,12 +1,11 @@
 def main():
     func = int(input('Graph maker = 1, Help/Explanation = 2: '))
-    answer = ""
+    answer = "y_{ 1}=".replace(' ','')
     match func:
         case 1:
             values = input('Input n and t in forms of "n:t,n:t" etc: ').split(',')
             for i in range(len(values)):
                 values[i] = values[i].split(':')
-            for i in range(len(values)):
                 answer += f'+g\left(x,{values[i][0]},{values[i][1]}\ right)'.replace(' ','')
             print(answer)
 
