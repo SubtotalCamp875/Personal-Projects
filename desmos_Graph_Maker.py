dbc3d4ea4db0caac3918502f3db1ee6601c0d3e9
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-10 16:36:15 +0000

    Mon, Jul 10, 2023, 4:36 PM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -1,5 +1,5 @@
 def main():
-    func = int(input('Graph maker = 1, Help/Explanation = 2: '))
+    func = int(input('Graph maker = 1, Pairing = 2, Help/Explanation = 3: '))
     answer = "y_{ 1}=".replace(' ','')
     steps, notes_list  = ['10','11','12','13','1','2','3','4','5','6','7','8','9'], ['a', 'a#', 'b', 'b#', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
     notes = dict(zip(notes_list,steps))
@@ -9,8 +9,12 @@ def main():
             values = input('Input n and t in forms of "n t,-n -t" etc: ').split(',')
             for i in range(len(values)):
                 values[i] = values[i].split(' ')
-                #replace letter with number
-                if values[i][0] in notes_list: values[i][0] = notes[values[i][0]]
+                #replace notes with number
+                if values[i][0] in notes_list:
+                    values[i][0] = notes[values[i][0]]
+                    #find difference between prev value and notes
+                    if i > 0:
+                        values[i][0] = int(values[i][0]) - int(values[i-1][0])
                 answer += f'+g\left(x,{values[i][0]},{values[i][1]}\ right)'.replace(' ','')
             print(f'\n{answer}\n')
 
