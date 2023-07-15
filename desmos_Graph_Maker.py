b43502c90fcd77963060928aae5ac6f9dabea501
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-15 15:25:33 +0000

    Sat, Jul 15, 2023, 3:25 PM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -44,7 +44,8 @@ def main():
 
         case 6:
             values = input('Input values for n and t (ex. [n t,-n -t] [5 6,-a -10] or visit Syntax Pairing): ').split(',')
-            shift, change_amount = input('What coord should the shift start on (ex. x=3, y=7)? '), input('How much should the graph shift by (ex. [x=5, y=-3] [x=10, y=8])? ')
+            change_amount, shift = input('How much should the graph shift by (ex. [x=5, y=-3] [x=10, y=8])? ').replace('x=', '').replace('y=', '').replace(' ','').split(','), input('What coord should the shift start on (ex. x=3, y=7)? ').replace('x=', '').replace('y=', '').replace(' ','').split(',')
+            print(change_amount, shift)
 
 
 def convert(values, mode):
