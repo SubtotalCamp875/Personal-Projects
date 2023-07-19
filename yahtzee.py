b112495842c0d5a4e3086fd637987cba2df98378
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-19 19:53:32 +0000

    Wed, Jul 19, 2023, 7:53 PM +00:00

--- yahtzee.py
+++ yahtzee.py
@@ -5,7 +5,7 @@ def main():
         action = input('("?" for help) Input: ')
 
         if action == "?":
-            print('\nType "n" when prompted for input to roll a new set of 5 dices\nor\ntype the the numbers rolled to reroll them. For example, if you rolled 1,2,3,3,6 type "3,6" to roll a single 3 and the 6.\nNote: please type the numbers ffrom smallest to largest in the syntax shown (with commas)\n')
+            print('\nType "n" when prompted for input to roll a new set of 5 dices\nor\ntype the the numbers rolled to reroll them. For example, if you rolled 1,2,3,3,6 type "3,6" to roll a single 3 and the 6.\nNote: Please type of the numbers using the syntax shown (with ocmmas). And randomness is random - which means you might end up having the same number. That is not a bug!\n')
 
         elif action == 'n':
             dice, reroll = [random.choice(range(6))+1, random.choice(range(6))+1, random.choice(range(6))+1, random.choice(range(6))+1, random.choice(range(6))+1], 2
@@ -13,6 +13,9 @@ def main():
             print(f'\nYou rolled a set of dice. Rerolls remaining: {reroll}\n==================================\n{dice}\n==================================\n')
 
         else:
+            if reroll == 0:
+                print('You have already used both of your rerolls. Please create a new set of dice by typing "n" when propted for input.')
+                continue
             action = action.replace(' ','').split(',')
             for i in range(len(action)):
                 for j in range(5):
@@ -20,6 +23,7 @@ def main():
                         dice[j] = random.choice(range(6))+1
                         break
             reroll -= 1
+            dice.sort()
             print(f'\nYou rolled a set of dice. Rerolls remaining: {reroll}\n==================================\n{dice}\n==================================\n')
 
 
