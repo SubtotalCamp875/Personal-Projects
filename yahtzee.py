59a3c0db2260f53fbc265ba379fd9018be245b4f
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-19 20:45:57 +0000

    Wed, Jul 19, 2023, 8:45 PM +00:00

--- yahtzee.py
+++ yahtzee.py
@@ -45,15 +45,13 @@ def calculations(dice):
 
 
     for i in range(5): #stores the amount fo numbers that are in a row within a list then reset
-        if i == 0: continue
+        if i == 0:
+            chain_list.append(chain)
+            continue
         if dice[i] == dice[i-1]:
             chain += 1
             if i != 4: continue
-        if dice[i] != dice[i-1] and chain >= 2:
-            chain_list.append(chain)
-            chain = 1
-            continue
-        print(i)
+        if dice[i] != dice[i-1] and chain >= 2: chain = 1
         chain_list.append(chain)
     print(chain_list)
 
