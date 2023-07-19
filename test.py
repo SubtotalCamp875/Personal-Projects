feece9c6a49fba9877daa7bb42845fb16b4a65d6
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-19 21:22:02 +0000

    Wed, Jul 19, 2023, 9:22 PM +00:00

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-x = [['1','2','3','4'],['2','3','4','5'],['3','4','5','6']]
-y = ['1','2','3','4','6']
-if str(x[0]).replace('[','').replace(']','') in str(y):
-    print(1)
-print(2)
+s_straights_list = [['1','2','3','4'],['2','3','4','5'],['3','4','5','6']]
+dice = ['1','2','3','4','6']
+print(str(s_straights_list[0]).replace('[','').replace(']',''))
+
+
+for i in range(3):
+    if str(s_straights_list[i]).replace('[','').replace(']','') in str(dice): print('Small straight for: 30\n')
