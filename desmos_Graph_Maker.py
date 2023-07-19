5a27753c6d5feeb0514c35e6957bc831980c0e86
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-19 13:53:25 +0000

    Wed, Jul 19, 2023, 1:53 PM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -73,7 +73,7 @@ def main():
                 print(difference(convert(input('Enter list of values (ex. 1,2,d#,e,5): ').lower(), 'n').replace(' ','').split(',')))
 
             case 7:
-                print( )
+                print('About the )
 
 def convert(values, mode):
     steps, notes_list  = ['10','11','12','13','1','2','3','4','5','6','7','8','9'], ['a', 'a#', 'b', 'b#', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
