70f04dbf7acc35f01d95ce59510720b9fa4733e3
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-19 21:23:03 +0000

    Wed, Jul 19, 2023, 9:23 PM +00:00

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 s_straights_list = [['1','2','3','4'],['2','3','4','5'],['3','4','5','6']]
-dice = ['1','2','3','4','6']
-print(str(s_straights_list[0]).replace('[','').replace(']',''))
+dice = ['1','2','3','4','4']
+print(dice)
 
 
 for i in range(3):
