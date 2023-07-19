e4293be7f29317890fc7627f9b82cf0350859e67
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-19 20:00:42 +0000

    Wed, Jul 19, 2023, 8:00 PM +00:00

--- yahtzee.py
+++ yahtzee.py
@@ -7,10 +7,13 @@ def main():
         if action == "?":
             print('\nType "n" when prompted for input to roll a new set of 5 dices\nor\ntype the the numbers rolled to reroll them. For example, if you rolled 1,2,3,3,6 type "3,6" to roll a single 3 and the 6.\nNote: Please type of the numbers using the syntax shown (with ocmmas). And randomness is random - which means you might end up having the same number. That is not a bug!\n')
 
+
         elif action == 'n':
             dice, reroll = [random.choice(range(6))+1, random.choice(range(6))+1, random.choice(range(6))+1, random.choice(range(6))+1, random.choice(range(6))+1], 2
             dice.sort()
             print(f'\nYou rolled a set of dice. Rerolls remaining: {reroll}\n==================================\n{dice}\n==================================\n')
+            print(calculations(dice))
+
 
         else:
             if reroll == 0:
@@ -25,14 +28,22 @@ def main():
             reroll -= 1
             dice.sort()
             print(f'\nYou rolled a set of dice. Rerolls remaining: {reroll}\n==================================\n{dice}\n==================================\n')
+            print(calculations(dice))
 
 
 
-
-
-
-def calculations():
-    pass
+def calculations(dice):
+    solutions, chain = 'With the above dices, you can score with:\n', 0
+    if '1' in dice: solutions += 'Aces\n'
+    if '2' in dice: solutions += 'Twos\n'
+    if '3' in dice: solutions += 'Threes\n'
+    if '4' in dice: solutions += 'Fours\n'
+    if '5' in dice: solutions += 'Fives\n'
+    if '6' in dice: solutions += 'Sixes\n'
+    for i in range(len(dice)):
+        if i == 0: pass
+        if dice[i] == dice[i-1]: chain += 1
+        else: chain = 0
 
 
 if __name__ == '__main__':
