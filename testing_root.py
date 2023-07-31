bd6f2a91aa5cc77c307fc968ebe2f40643840fa2
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-31 20:27:44 +0000

    Mon, Jul 31, 2023, 8:27 PM +00:00

--- testing_root.py
+++ testing_root.py
@@ -6,11 +6,10 @@ def main():
         value, power = random.choice(range(1,10)), random.choice(range(2,4))
         answer = input(f'{value}^{power} = ')
         if answer == "e": break
-        if innumberic(answer) == False: print(f'{value}^{power} = {value**power}')
-        if int(answer) == value**power:
+        if answer.isnumeric() == False or int(answer) != value**power: print(f'{value}^{power} = {value**power}')
+        else:
             total += 1
             continue
-        else: print(f'{value}^{power} = {value**power}')
     print(f'{Total}/25')
 
 if __name__ == '__main__':
