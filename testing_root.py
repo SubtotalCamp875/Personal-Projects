27457cef2e4deb5acd31a74dabd7ad7266edf729
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-31 20:15:37 +0000

    Mon, Jul 31, 2023, 8:15 PM +00:00

--- testing_root.py
+++ testing_root.py
@@ -3,10 +3,10 @@ import random
 def main():
     while True:
         value = random.choice(range(1,10))
-        print(value)
-        break
-
-
+        answer = input(f'{value}^2 = ')
+        if answer == "e": break
+        if int(answer) == value**2: continue
+        else: print(f'{value}^2 = {value**2}')
 
 if __name__ == '__main__':
     main()
