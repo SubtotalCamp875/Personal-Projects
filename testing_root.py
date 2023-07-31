26dceb288b6d2a6bcb8aa97d357ac10d1cddeacf
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-31 20:19:41 +0000

    Mon, Jul 31, 2023, 8:19 PM +00:00

--- testing_root.py
+++ testing_root.py
@@ -3,13 +3,14 @@ import random
 def main():
     total = 0
     for i in range(25):
-        value = random.choice(range(1,10))
-        answer = input(f'{value}^2 = ')
+        value, power = random.choice(range(1,10)), random.choice(range(2,3))
+        answer = input(f'{value}^{power} = ')
         if answer == "e": break
-        if int(answer) == value**2:
+        if int(answer) == value**power:
             total += 1
             continue
-        else: print(f'{value}^2 = {value**2}')
+        else: print(f'{value}^{power} = {value**power}')
+    print(f'{Total}/25')
 
 if __name__ == '__main__':
     main()
