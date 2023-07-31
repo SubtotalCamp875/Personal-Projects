e548f04785eb07187a0579b9913166d8c21b2cb7
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-31 02:25:28 +0000

    Mon, Jul 31, 2023, 2:25 AM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -28,7 +28,9 @@ def main():
                     if values[i][0].lower() in notes_list:
                         values[i][0] = notes[values[i][0].lower()]
                         #find difference between prev value and notes when notes is not the first value
-                        if i > 0: values[i][0] = int(values[i][0]) - current
+                        if i > 0:
+                            values[i][0] = int(values[i][0]) - current
+                            current += int(values[i][0])
                     answer += f'+g\left(x,{values[i][0]},{values[i][1]}\ right)'.replace(' ','')
 
                 print(f'\n{answer}\n')
@@ -50,7 +52,9 @@ def main():
                     if values[i][0].lower() in notes_list:
                         values[i][0] = notes[values[i][0].lower()]
                         #find difference between prev value and notes when notes is not the first value
-                        if i > 0: values[i][0] = int(values[i][0]) - current
+                        if i > 0:
+                            values[i][0] = int(values[i][0]) - current
+                            current += int(values[i][0])
                     if i+1 >= int(shift[x]): values[i][x] = int(values[i][x]) + int(change_amount[x])
                     if i+1 >= int(shift[y]): values[i][y] = float(values[i][y]) + float(change_amount[y])
                     answer += f'+g\left(x,{values[i][x]},{values[i][y]}\ right)'.replace(' ','')
