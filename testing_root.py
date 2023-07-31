111503ef310872b75cbbe62f3df38a5966eb4908
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-31 20:20:18 +0000

    Mon, Jul 31, 2023, 8:20 PM +00:00

--- testing_root.py
+++ testing_root.py
@@ -3,7 +3,7 @@ import random
 def main():
     total = 0
     for i in range(25):
-        value, power = random.choice(range(1,10)), random.choice(range(2,3))
+        value, power = random.choice(range(1,10)), random.choice(range(2,4))
         answer = input(f'{value}^{power} = ')
         if answer == "e": break
         if int(answer) == value**power:
