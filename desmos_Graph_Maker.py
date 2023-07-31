167e19d246ce2914eb671456e6c9904cccfa99bf
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-31 01:48:39 +0000

    Mon, Jul 31, 2023, 1:48 AM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -14,7 +14,7 @@ def main():
 
         match func:
             case 1: #Graph Maker
-                values = input('Input values for n and t (ex. [n t,-n t] [5 6,a 10] or visit Syntax Pairing by inputing "v"): ').split(',')
+                values = input('Input values for n and t (ex. [n t,-n t.0] [-5 6,a 10.5] or visit Syntax Pairing by inputing "v"): ').split(',')
                 if values[0] == 'v':
                     skip, func = True, 'v'
                     print('Switching to Syntax pairing (2) now...\n')
@@ -34,7 +34,7 @@ def main():
                 print(values)
 
             case 2:
-                values, x, y = input('Input values for n and t (ex. [n t,-n t] [5 6,-a 10] or visit Syntax Pairing by inputing "v"): ').split(','), 0, 1
+                values, x, y = input('Input values for n and t (ex. [n t,-n t.0] [-5 6,a 10.5] or visit Syntax Pairing by inputing "v"): ').split(','), 0, 1
                 if values[0] == 'v':
                     skip, func = True, 'v'
                     print('Switching to Syntax pairing (2) now...\n')
@@ -78,7 +78,7 @@ def main():
                 print(Fore.GREEN+'\nAbout this project - The first version of this project was created when I, SubtotalCamp875, along with YackaDesmos wanted to create a Rickroll using Desmos Graphing Calculator. I then created this project to further optimize the code and features as it was easier to rewrite the code compared to modifying its whole i/o and syntax system. We also created our own discord server with the help of Playli750! The system also surports notes like a,a#,b,...,g,g# (calculated by Yacka). Note that the code is not perfect and might contain bugs and also requires the user to use the correct syntax or else it will raise an error. If you find any bugs or would like to discuss anything, Join the Discord Server!\n'+Fore.YELLOW+'The Original Video by SubtotalCamp875: '+Fore.BLUE+'https://youtu.be/JtP_3Jao910 \n'+Fore.YELLOW+'YackaDesmos Channel: '+Fore.BLUE+'https://www.youtube.com/@YackalipsDesmos/about \n'+Fore.YELLOW+'Playli750 channel: '+Fore.BLUE+'https://www.youtube.com/@playli750/about \n'+Fore.YELLOW+'The Desmos Graphing Server (Discord): '+Fore.BLUE+'https://discord.gg/rQrnMtJmsv \n')
 
                 time.sleep(2)
-                print(Fore.GREEN+'Mode 1 - The first mode, also the main mode, requires the syntax [n t,-n t] [5 6,-a -10] where the n value is the change in height at the location for t (on the x coordinate). The input can be numbers, notes, negetives, or all the above.\n'+Fore.YELLOW+'So if n=5 and t=6, there will be a increase of 5 in height from (6,0) to (6,5) on a graph making it look like a backwards "L".\n'+Fore.RED+'The next set of numbers (seperated by the comma) increased at the next given x coordinate (t) but increase/decrease based in the previous value of 5.\n'+Fore.WHITE+'The mode also supports "v" which will automatically bring you to Mode 3 to help you create the syntax.\n')
+                print(Fore.GREEN+'Mode 1 - The first mode, also the main mode, requires the syntax [n t,-n t.0] [-5 6,a 10.5] where the n value is the change in height at the location for t (on the x coordinate). The input can be numbers, notes, negetives, or all the above.\n'+Fore.YELLOW+'So if n=5 and t=6, there will be a increase of 5 in height from (6,0) to (6,5) on a graph making it look like a backwards "L".\n'+Fore.RED+'The next set of numbers (seperated by the comma) increased at the next given x coordinate (t) but increase/decrease based in the previous value of 5.\n'+Fore.WHITE+'The mode also supports "v" which will automatically bring you to Mode 3 to help you create the syntax.\n')
 
                 time.sleep(2)
                 print(Fore.GREEN+'Mode 2 - Mode 2 is the same as Mode 1 in the sense that is ask you for the same inputs as Mode 1 and also supports an input of "v" but its main purpose is to shift the graph by a certain amount.\n'+Fore.YELLOW+'When asked the second question (How much should the graph shift by?) you are required to enter in the syntax of "x=5, y=-3" or "x=10, y=8" where it can be any number or note. The amount stated will be the amount the pionts on the graph shift by. If there was a piont at (1,0) and "x=3, y=0" then the result will be (4,0).\n'+Fore.RED+'The third questions (What coord should the shift start on?) requires the same syntax as question 2 BUT this time, the numbers define where the change in starting. The numbers are based on the pionts you gave and not the location on a graph. If you inputted pionts on (5,0), (6,5), (7,3), then if x=1, the change in x will start at (5,0) and also increases the x value of all pionts after.\n'+Fore.WHITE+'The mode also supports "v" which will automatically bring you to Mode 3 to help you create the syntax.\n')
