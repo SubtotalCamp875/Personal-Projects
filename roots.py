00ee2e4c3d753ab868fff1b4825ce5f403e5f477
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-07-31 20:03:14 +0000

    Mon, Jul 31, 2023, 8:03 PM +00:00

--- roots.py
+++ roots.py
@@ -1,7 +1,6 @@
 def main():
     for i in range(3):
-        for j in range(10):
-            print(f'{j} = {j**(i+1)}')
+        for j in range(10): print(f'{j} = {j**(i+1)}')
         sep()
 
 def sep(): print('============')
