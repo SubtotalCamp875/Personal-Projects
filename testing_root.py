f737d3f14e8a46695575631da392bd1bc03cae97
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-08-02 12:02:19 +0000

    Wed, Aug 2, 2023, 12:02 PM +00:00

--- testing_root.py
+++ testing_root.py
@@ -1,15 +1,47 @@
 import random
 
 def main():
+    diff = input('Difficukty: ')
     total = 0
-    for i in range(25):
-        value, power = random.choice(range(1,10)), random.choice(range(2,4))
-        answer = input(f'{value}^{power} = ')
-        if answer == "e": break
-        if answer.isnumeric() == False or int(answer) != value**power: print(f'{value}^{power} = {value**power}')
-        else:
-            total += 1
-            continue
+
+    match diff:
+        case 'E'|'e'|'easy':
+            for i in range(25):
+                value, power = random.choice(range(1,10)), random.choice(range(2,4))
+                answer = input(f'{value}^{power} = ')
+                if answer == "e": break
+                if answer.isnumeric() == False or int(answer) != value**power: print(f'{value}^{power} = {value**power}')
+                else:
+                    total += 1
+                    continue
+
+        case 'm'|'M'|'medium':
+            for i in range(25):
+                power = random.choice(range(2,4))
+                if power == 2: value = random.choice(range(11,21))
+                if power == 3: value = random.choice(range(2,10))
+
+                answer = input(f'{value}^{power} = ')
+                if answer == "e": break
+                if answer.isnumeric() == False or int(answer) != value**power: print(f'{value}^{power} = {value**power}')
+                else:
+                    total += 1
+                    continue
+
+        case 'H'|'h'|'hard':
+            for i in range(25):
+                power = random.choice(range(2,4))
+                if power == 2: value = random.choice(range(11,21))
+                if power == 3: value = random.choice(range(11,21))
+
+                answer = input(f'{value}^{power} = ')
+                if answer == "e": break
+                if answer.isnumeric() == False or int(answer) != value**power: print(f'{value}^{power} = {value**power}')
+                else:
+                    total += 1
+                    continue
+
+
     print(f'{total}/25')
 
 if __name__ == '__main__':
