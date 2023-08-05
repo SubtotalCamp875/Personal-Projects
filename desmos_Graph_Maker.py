2984e64907cf2cb8af2237e3018b9f33ce86f95f
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-08-05 22:52:50 +0000

    Sat, Aug 5, 2023, 10:52 PM +00:00

--- desmos_Graph_Maker.py
+++ desmos_Graph_Maker.py
@@ -55,6 +55,7 @@ def main():
                         if i > 0:
                             values[i][0] = int(values[i][0]) - current
                             current += int(values[i][0])
+                    print(values[i],change_amount[x],change_amount[y])
                     if i+1 >= int(shift[x]): values[i][x] =  int(values[i][x]) + int(change_amount[x])
                     if i+1 >= int(shift[y]): values[i][y] = float(values[i][y]) + float(change_amount[y])
                     print(values[i],change_amount[x],change_amount[y])
