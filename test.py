77d4beb0ab7389fe00c096646e46f8c975a4ddc0
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-08-05 21:34:53 +0000

    Sat, Aug 5, 2023, 9:34 PM +00:00

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 
 steps, notes_list  = ['0','1','2','3','4','5','6','7','8','9','10','11','12'], ['e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e2']
 for i in range(len(steps)):
-    print(f'{steps[i]},{notes_list[i]}')
+    print(f'{steps[i]} = {notes_list[i]}')
