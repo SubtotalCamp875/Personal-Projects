691917e0826eb24fc8f15650d22d9d7f73d45d06
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-08-06 19:32:39 +0000

    Sun, Aug 6, 2023, 7:32 PM +00:00

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
+import string
 
-dict(zip(,range(52)))
+value, answer = 'jSHRt6QbkZY', ''
+all = dict(zip(string.ascii_letters,range(52)))
+for i in range(len(value)):
+    if value[i].isnumeric() == True:
+        answer += f'{value[i]}, '
+    else:
+        answer += f'{all(value[i])}, '
+print(answer)
