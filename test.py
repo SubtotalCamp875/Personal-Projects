c172655fb1bc33ed33fd01273dc17e0ae1bea728
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-08-06 19:46:54 +0000

    Sun, Aug 6, 2023, 7:46 PM +00:00

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import string
 
 value, answer = 'jSHRt6QbkZY', ''
-all = dict(zip(string.ascii_letters,range(52)))
+all = dict(zip(string.ascii_letters,range(53)))
 print(all)
 for i in range(len(value)):
     if value[i].isnumeric() == True: answer += f'{value[i]}, '
