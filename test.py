66547ff0cf24ab8579e62266ee2632c0207022b6
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-08-06 19:37:05 +0000

    Sun, Aug 6, 2023, 7:37 PM +00:00

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ all = dict(zip(string.ascii_letters,range(52)))
 for i in range(len(value)):
     if value[i].isnumeric() == True:
         answer += f'{value[i]}, '
-    else:
-        answer += f'{all(value[i])}, '
+    print(value[i])
+    else: answer += f'{all(value[i])}, '
 print(answer)
