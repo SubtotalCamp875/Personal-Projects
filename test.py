eddbdb582295b5ec640723aecc0a027793d50413
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2023-08-06 19:54:27 +0000

    Sun, Aug 6, 2023, 7:54 PM +00:00

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 import string
 
-value, answer = 'jSHRt6QbkZY', ''
 all = dict(zip(string.ascii_letters,range(53)))
-print(all)
-for i in range(len(value)):
-    if value[i].isnumeric() == True: answer += ' '
-    else: answer += f'{all[value[i]]}'
-print(answer)
+
+def decode(value, all):
+    answer = ''
+
+    for i in range(len(value)):
+        if value[i].isnumeric() == True: answer += ' '
+        else: answer += f'{all[value[i]]}'
+    print(answer)
+
+
+decode('jSHRt6QbkZY', all)
+decode('North', all)
+decode('South', all)
+decode('East', all)
+decode('West', all)
