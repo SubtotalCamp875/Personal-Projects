4c82e861d3f879393674c481cee6cf96905d6ee3
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2024-09-08 03:47:42 +0000

    Sun, Sep 8, 2024, 3:47 AM +00:00

--- test.py
+++ test.py
@@ -1,2 +1,26 @@
-n = int(input("How many sheeps? "))
-for i in range(n): print('🐑'*n)
+def main():
+    itemList = [
+        'netherite_helmet',
+        'netherite_chestplate',
+        'netherite_leggings',
+        'netherite_boots',
+    ]
+    finalOutput = []
+
+
+    for i in range(len(itemList)):
+
+        armorValue = input(f'Set a armor value for {itemList[i]}: ')
+        if 'helmet' in itemList[i]: armorSlot, uuid = 'head', '2ad3f246-fee1-4e67-b886-69fd380bb150'
+        elif 'chestplate' in itemList[i]: armorSlot, uuid = 'chest', '9f3d476d-c118-4544-8365-64846904b48e'
+        elif 'leggings' in itemList[i]: armorSlot, uuid = 'legs', 'd8499b04-0e66-4726-ab29-64469d734e0d'
+        elif 'boots' in itemList[i]: armorSlot, uuid = 'feet', '845db27c-c624-495f-8c9f-6020a9a58b6b'
+
+        finalOutput.append(f'<item:minecraft:{itemList[i]}>.anyDamage().addGlobalAttributeModifier(<attribute:minecraft:generic.armor>, "{uuid}", "New Armor", {armorValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>]);')
+
+    for i in range(len(finalOutput)): print(finalOutput[i])
+
+
+
+if __name__ == '__main__':
+    main()
