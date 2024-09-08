9ac6908994cd425157f83ad273047a4f80a03d33
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2024-09-08 16:17:00 +0000

    Sun, Sep 8, 2024, 4:17 PM +00:00

--- test.py
+++ test.py
@@ -154,7 +154,10 @@ def main():
         elif 'leggings' in itemList[i]: armorSlot, uuid = 'legs', 'd8499b04-0e66-4726-ab29-64469d734e0d'
         elif 'boots' in itemList[i]: armorSlot, uuid = 'feet', '845db27c-c624-495f-8c9f-6020a9a58b6b'
 
-        finalOutput.append(f'<item:{itemList[i]}>.anyDamage().addGlobalAttributeModifier(<attribute:minecraft:generic.armor>, "{uuid}", "New Armor", {armorValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>]);')
+        finalOutput.append(f'<item:{itemList[i]}>.anyDamage()
+            .addGlobalAttributeModifier(<attribute:minecraft:generic.armor>, "{uuid}", "New Armor", {armorValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>])
+            .addGlobalAttributeModifier(<attribute:minecraft:generic.armor>, "{uuid}", "New Armor", {armorValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>])
+            .addGlobalAttributeModifier(<attribute:minecraft:generic.armor>, "{uuid}", "New Armor", {armorValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>]);')
 
     for i in range(len(finalOutput)): print(finalOutput[i])
 
