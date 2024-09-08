e5df8c7f11ba9496b6a8fbfffa3f9316a5e293bd
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2024-09-08 16:24:08 +0000

    Sun, Sep 8, 2024, 4:24 PM +00:00

--- test.py
+++ test.py
@@ -148,16 +148,17 @@ def main():
 
     for i in range(len(itemList)):
 
-        armorValue = input(f'Set a generic armor value for {itemList[i]}: ')
+        armorValue, kbResValue, armorValue = '', '', ''
+        while armorValue == '': armorValue = input(f'Set a generic armor value for {itemList[i]}: ')
+        while kbResValue == '': kbResValue = input(f'Set a generic kb resistance value for {itemList[i]}: ')
+        while armorToughnessValue == '': armorToughnessValue = input(f'Set a generic toughness value for {itemList[i]}: ')
+
         if 'helmet' in itemList[i]: armorSlot, uuid = 'head', '2ad3f246-fee1-4e67-b886-69fd380bb150'
         elif 'chestplate' in itemList[i]: armorSlot, uuid = 'chest', '9f3d476d-c118-4544-8365-64846904b48e'
         elif 'leggings' in itemList[i]: armorSlot, uuid = 'legs', 'd8499b04-0e66-4726-ab29-64469d734e0d'
         elif 'boots' in itemList[i]: armorSlot, uuid = 'feet', '845db27c-c624-495f-8c9f-6020a9a58b6b'
 
-        finalOutput.append(f'<item:{itemList[i]}>.anyDamage()
-            .addGlobalAttributeModifier(<attribute:minecraft:generic.armor>, "{uuid}", "New Armor", {armorValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>])
-            .addGlobalAttributeModifier(<attribute:minecraft:generic.armor>, "{uuid}", "New Armor", {armorValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>])
-            .addGlobalAttributeModifier(<attribute:minecraft:generic.armor>, "{uuid}", "New Armor", {armorValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>]);')
+        finalOutput.append(f'<item:{itemList[i]}>.anyDamage().addGlobalAttributeModifier(<attribute:minecraft:generic.armor>, "{uuid}", "New Armor", {armorValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>]).addGlobalAttributeModifier(<attribute:minecraft:generic.knockback_resistance>, "{uuid}", "New Armor", {kbResValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>]).addGlobalAttributeModifier(<attribute:minecraft:generic.armor_toughness>, "{uuid}", "New Armor", {armorToughnessValue}, AttributeOperation.ADDITION, [<constant:minecraft:equipmentslot:{armorSlot}>]);')
 
     for i in range(len(finalOutput)): print(finalOutput[i])
 
