42599a5e1a0bd394e0b98f8727a7fbc17189e6b9
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2024-09-11 19:44:45 +0000

    Wed, Sep 11, 2024, 7:44 PM +00:00

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 def main():
     itemList = [
         "knightquest:apple_helmet",
-        "knightquest:apple_chestplate"]
-    """
+        "knightquest:apple_chestplate",
         "knightquest:apple_leggings",
         "knightquest:apple_boots",
         "knightquest:bamboo_blue_helmet",
@@ -142,8 +141,8 @@ def main():
         "knightquest:skulk_helmet",
         "knightquest:skulk2_helmet",
         "knightquest:skulk3_helmet",
-        "knightquest:skulk4_helmet",
-    """
+        "knightquest:skulk4_helmet"
+    ]
 
     finalOutput = []
 
