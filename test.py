6168ef1c1f492a056a68cb9284e34183368e66eb
SubtotalCamp875 <106037298+SubtotalCamp875@users.noreply.github.com>
2024-09-14 15:51:17 +0000

    Sat, Sep 14, 2024, 3:51 PM +00:00

--- test.py
+++ test.py
@@ -145,7 +145,7 @@ def main():
         "knightquest:skulk4_helmet"
     """
 
-    finalOutput, changeArmorFunction = [],[]
+    changeArmorFunction = []
 
 
     for i in range(len(itemList)):
@@ -163,19 +163,11 @@ def main():
 
         changeArmorFunction.append(f'changeArmor(<item:{itemList[i]}>, {armorValue}, {armorToughnessValue}, {kbResValue});')
 
-    finalOutput.append('}')
-    for i in range(len(finalOutput)): print(finalOutput[i])
-    for i in range(len(finalOutput)): print(changeArmorFunction[i])
+    print('function changeArmor(item as IItemStack, armor as double, toughness as double, resistance as double) as void {\n    item.definition.addAttributeModifier(<attribute:minecraft:generic.armor>, AttributeModifier.create(<resource:minecraft:armor.helmet>, armor, <constant:minecraft:attribute/operation:add_value>), <constant:minecraft:equipmentslot/group:head>);\n    item.definition.addAttributeModifier(<attribute:minecraft:generic.knockback_resistance>, AttributeModifier.create(<resource:minecraft:armor.helmet>, resistance, <constant:minecraft:attribute/operation:add_value>), <constant:minecraft:equipmentslot/group:head>);\n    item.definition.addAttributeModifier(<attribute:minecraft:generic.armor_toughness>, AttributeModifier.create(<resource:minecraft:armor.helmet>, toughness, <constant:minecraft:attribute/operation:add_value>), <constant:minecraft:equipmentslot/group:head>);\n}')
+    for i in range(len(changeArmorFunction)): print(changeArmorFunction[i])
 
 
 
 
 if __name__ == '__main__':
     main()
-
-"""
-function changeArmor(item as IItemStack, armor as double, toughness as double, resistance as double) as void {
-    item.definition.addAttributeModifier(<attribute:minecraft:generic.armor>, AttributeModifier.create(<resource:minecraft:armor.helmet>, armor, <constant:minecraft:attribute/operation:add_value>), <constant:minecraft:equipmentslot/group:head>);
-    item.definition.addAttributeModifier(<attribute:minecraft:generic.knockback_resistance>, AttributeModifier.create(<resource:minecraft:armor.helmet>, resistance, <constant:minecraft:attribute/operation:add_value>), <constant:minecraft:equipmentslot/group:head>);
-    item.definition.addAttributeModifier(<attribute:minecraft:generic.armor_toughness>, AttributeModifier.create(<resource:minecraft:armor.helmet>, toughness, <constant:minecraft:attribute/operation:add_value>), <constant:minecraft:equipmentslot/group:head>);
-}
